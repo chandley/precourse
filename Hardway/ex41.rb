4dc53d27165a81441daf61bf716f34e493616a8c
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-09-28 21:55:56 +0100

    ex41.rb fixed

--- Hardway/ex41.rb
+++ Hardway/ex41.rb
@@ -33,9 +33,9 @@ def craft_names(rand_words, snippet, pattern ,caps=false)
 end
 
     
-def craft_paraks(rand_words, snippet, pattern)
+def craft_params(rand_words, snippet, pattern)
   names = (0...snippet.scan(pattern).length).map do
-    craft_params = rand (3) +1
+    param_count = rand (3) +1
     params = (0...param_count).map {|x| rand_words.pop() }
     params.join(', ')
   end
@@ -47,8 +47,8 @@ end
 def convert(snippet, phrase)
   rand_words = WORDS.sort_by {rand}
   class_names = craft_names(rand_words, snippet, /###/, caps=true)
-  class_names = craft_names(rand_words, snippet, /\*\*\*/)
-  class_names = craft_names(rand_words, snippet, /@@@/)
+  other_names = craft_names(rand_words, snippet, /\*\*\*/)
+  param_names = craft_params(rand_words, snippet, /@@@/)
 
   results = []
 
@@ -75,7 +75,7 @@ loop do
       question, answer = answer, question
     end
 
-    print question "\n\n> "
+    print question, "\n\n> "
     exit (0) unless $stdin.gets
     puts "\nANSWER: %s\n\n" % answer
   end
